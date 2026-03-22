e265f064d8b1425294742c90c7fcc141e7f61c2c
Daniel Morante <daniel@morante.net>
2026-03-22 04:24:38 -0400

    Fix FreeBSD build: use struct member access (.) instead of pointer (->) for timeval in cgtimer_time

--- util.c
+++ util.c
@@ -1517,8 +1517,8 @@ void cgtimer_time(cgtimer_t *ts_start)
 	struct timeval tv;
 
 	cgtime(&tv);
-	ts_start->tv_sec = tv->tv_sec;
-	ts_start->tv_nsec = tv->tv_usec * 1000;
+	ts_start->tv_sec = tv.tv_sec;
+	ts_start->tv_nsec = tv.tv_usec * 1000;
 }
 #endif /* __MACH__ */
 
